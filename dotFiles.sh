2b186442291904d3e1c84228a8f5aaa7311723cc
othonalberto <othonalberto@gmail.com>
2017-07-21 15:03:49 -0300

    mais curto

--- dotFiles.sh
+++ dotFiles.sh
@@ -1,11 +1,11 @@
-# @othnalberto
+# @othonalberto
 
 # copia .vimrc para repositorio do github
-cp ~/.vimrc ~/repositorios/dotfiles
-cd ~/repositorios/dotfiles
-mv .vimrc vimrc
+cp ~/.vimrc ~/repositorios/github/dotfiles/vimrc
 
 # copia .gitconfig para repositorio do github
-cp ~/.gitconfig ~/repositorios/dotfiles
-cd ~/repositorios/dotfiles
-mv .gitconfig gitconfig
+cp ~/.gitconfig ~/repositorios/github/dotfiles/gitconfig
+
+#copia config.fish para o repositorio do github
+cp ~/.config/fish/config.fish ~/repositorios/github/dotfiles
+
